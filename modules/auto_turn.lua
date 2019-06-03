75b6c2cb8414b2eb6d6a9a44d7c43594715164f9
htordeux <htordeux@laposte.net>
2019-06-03 10:22:07 +0200

    detab

--- modules/auto_turn.lua
+++ modules/auto_turn.lua
@@ -42,13 +42,13 @@ end)
 ]]--
 
 function removeBagItems()
-	for bag = 0,4,1 do
-		for slot = 1, GetContainerNumSlots(bag), 1 do
-			local name = GetContainerItemLink(bag,slot)
-			if name and string.find(name,"ff9d9d9d") then 
-				PickupContainerItem(bag,slot)
-				DeleteCursorItem()
-			end
-		end 
-	end
+    for bag = 0,4,1 do
+        for slot = 1, GetContainerNumSlots(bag), 1 do
+            local name = GetContainerItemLink(bag,slot)
+            if name and string.find(name,"ff9d9d9d") then 
+                PickupContainerItem(bag,slot)
+                DeleteCursorItem()
+            end
+        end 
+    end
 end
